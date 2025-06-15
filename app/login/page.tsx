52d0da5aa88a63e114e6ee4f9f43a4946ba1b77b
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-14 22:42:19 -0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,1 +1,14 @@
+// app/login/page.tsx
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+}
 
