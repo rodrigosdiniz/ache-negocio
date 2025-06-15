f5a5f19abe346766dc528a524d1ed61d9397110c
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-15 15:56:00 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from './components/Hero'
 import Features from './components/Features'
 import SearchSection from './components/SearchSection'
 import CallToAction from './components/CallToAction'
+import BusinessList from './components/BusinessList'
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
       <Features />
       <SearchSection />
       <CallToAction />
+      <BusinessList />
     </main>
   );
 }
+
