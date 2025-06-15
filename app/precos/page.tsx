8e9ee4854812578d7e6eb7d4489086bab756dfd5
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-14 22:41:36 -0300

    Update page.tsx

--- app/precos/page.tsx
+++ app/precos/page.tsx
@@ -1,1 +1,14 @@
+// app/precos/page.tsx
+export default function PrecosPage() {
+  return (
+    <div>
+      <h1>Escolha seu plano</h1>
+      <ul>
+        <li>Plano Básico - R$49/mês</li>
+        <li>Plano Profissional - R$99/mês</li>
+        <li>Plano Corporativo - R$199/mês</li>
+      </ul>
+    </div>
+  );
+}
 
