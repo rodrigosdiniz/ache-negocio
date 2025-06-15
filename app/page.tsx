c0cec7d471bd14227908211065edc3b7c3293124
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-15 12:18:36 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Hero from './components/Hero'
+import Features from './components/Features'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Features />
     </main>
   );
 }
+
 
