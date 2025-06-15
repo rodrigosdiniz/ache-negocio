e9330135547b6dffd45246c009056525b62422eb
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-14 22:34:02 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,3 +17,15 @@ export default function Navbar() {
     </nav>
   )
 }
+import Navbar from './components/Navbar'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
