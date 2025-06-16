102a0c9688bfa5cb612cb5adb6f3502e5f533dab
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-16 17:11:09 -0300

    Update Card.tsx

--- app/components/ui/Card.tsx
+++ app/components/ui/Card.tsx
@@ -1,6 +1,6 @@
 // app/components/ui/card.tsx
 import * as React from "react";
-import { cn } from "@/lib/utils";
+import { cn } from '../../lib/utils'
 
 const Card = React.forwardRef<
   HTMLDivElement,
