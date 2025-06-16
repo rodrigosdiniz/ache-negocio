dedf5f8a817b9aa00a4088e16486d0039d8561c1
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-16 13:16:13 -0300

    Update CompanyCard.tsx

--- app/components/CompanyCard.tsx
+++ app/components/CompanyCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/Card';
 import { PhoneIcon, StarIcon } from 'lucide-react';
 import Image from 'next/image';
 
