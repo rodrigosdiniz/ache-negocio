e7ed12d124578d58c9d5721db2d7eafb57d92bc9
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-20 01:19:53 -0300

    Update LogoutButton.tsx

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -3,37 +3,28 @@
 
 import { supabase } from '@/lib/supabase'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import Toast from '@/components/Toast'
+import { useToast } from '@/context/toast-context'
 
 export default function LogoutButton() {
   const router = useRouter()
-  const [mensagem, setMensagem] = useState('')
-  const [tipo, setTipo] = useState<'success' | 'error'>('success')
+  const { showToast } = useToast()
 
   const logout = async () => {
     const { error } = await supabase.auth.signOut()
     if (error) {
-      setMensagem('Erro ao sair')
-      setTipo('error')
+      showToast('Erro ao sair', 'error')
     } else {
-      setMensagem('Você saiu com sucesso')
-      setTipo('success')
+      showToast('Você saiu com sucesso', 'success')
       setTimeout(() => router.push('/login'), 2000)
     }
   }
 
   return (
-    <div className="relative">
-      <button
-        onClick={logout}
-        className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded shadow"
-      >
-        Sair
-      </button>
-      {mensagem && (
-        <Toast message={mensagem} type={tipo} onClose={() => setMensagem('')} />
-      )}
-    </div>
+    <button
+      onClick={logout}
+      className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded shadow"
+    >
+      Sair
+    </button>
   )
 }
