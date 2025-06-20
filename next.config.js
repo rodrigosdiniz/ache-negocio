ca8118e8436a030bdaa43cae8bb7669de9f927a9
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-20 20:43:58 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-};
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: [
+      'files.stripe.com',
+      'lh3.googleusercontent.com', // caso use login com Google
+      'jcnvpcjgydavwmedtdih.supabase.co', // imagens do Supabase
+      'cdn.resend.com', // se usar Resend com imagens
+    ],
+  },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+}
+
+module.exports = nextConfig
