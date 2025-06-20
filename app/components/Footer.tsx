830314a646f6f6ae52fb93bc9981df7ea2038371
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-20 16:35:28 -0300

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import Link from 'next/link'
+
 export default function Footer() {
   return (
     <footer style={{
@@ -8,20 +12,25 @@ export default function Footer() {
       marginTop: '2rem'
     }}>
       <p><strong>Ache Negócio SaaS</strong> © {new Date().getFullYear()}</p>
+
       <p>
         Desenvolvido por Rodrigo Diniz ·
         <a href="mailto:contato@achenegocio.com.br" style={{ marginLeft: '0.3rem', color: '#0070f3' }}>
           contato@achenegocio.com.br
         </a>
       </p>
+
       <p>
         <a href="https://www.instagram.com/nexu.sengenharia1/" target="_blank" rel="noopener noreferrer">
           Instagram
         </a> ·{' '}
         <a href="https://www.facebook.com/profile.php?id=61568743817647" target="_blank" rel="noopener noreferrer">
           Facebook
-        </a>
+        </a> ·{' '}
+        <Link href="/contato" style={{ color: '#0070f3' }}>
+          Fale Conosco
+        </Link>
       </p>
     </footer>
-  );
+  )
 }
