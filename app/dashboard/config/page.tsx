b96c568df8f664e84047b66b411a8ee936c196a0
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-19 22:29:44 -0300

    Create page.tsx

---
+++ app/dashboard/config/page.tsx
@@ -0,0 +1,8 @@
+export default function ConfiguracoesPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Configurações</h1>
+      <p>Preferências e opções personalizadas virão aqui.</p>
+    </div>
+  )
+}
