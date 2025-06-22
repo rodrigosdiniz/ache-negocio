f17e9bc0cb8174c81796b4804d9d6638c793439f
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-22 18:39:29 -0300

    Update textarea.tsx

--- app/components/ui/textarea.tsx
+++ app/components/ui/textarea.tsx
@@ -1,18 +1,12 @@
-// app/components/ui/textarea.tsx
-import { TextareaHTMLAttributes, forwardRef } from 'react'
+'use client'
 
-interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+import { TextareaHTMLAttributes } from 'react'
 
-export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <textarea
-        ref={ref}
-        className={`border border-gray-300 rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400 ${className}`}
-        {...props}
-      />
-    )
-  }
-)
-
-Textarea.displayName = 'Textarea'
+export function Textarea(props: TextareaHTMLAttributes<HTMLTextAreaElement>) {
+  return (
+    <textarea
+      {...props}
+      className={`w-full border border-gray-300 rounded px-3 py-2 ${props.className || ''}`}
+    />
+  )
+}
