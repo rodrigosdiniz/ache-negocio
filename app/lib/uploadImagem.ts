f10889dfdae7ae77ec36254343fe693fbadc76e8
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-22 18:14:06 -0300

    Update uploadImagem.ts

--- app/lib/uploadImagem.ts
+++ app/lib/uploadImagem.ts
@@ -1,13 +1,21 @@
-// lib/uploadImagem.ts
-import { supabase } from './supabase'
+// app/lib/uploadImagem.ts
+'use server'
+
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
 export async function uploadImagemEmpresa(empresaId: number, file: File) {
+  const supabase = createServerActionClient({ cookies })
+
   const fileExt = file.name.split('.').pop()
   const fileName = `${empresaId}.${fileExt}`
-  const { data, error } = await supabase.storage.from('empresas').upload(fileName, file, {
-    upsert: true,
-    contentType: file.type
-  })
+
+  const { data, error } = await supabase.storage
+    .from('empresas')
+    .upload(fileName, file, {
+      upsert: true,
+      contentType: file.type,
+    })
 
   if (error) throw error
 
