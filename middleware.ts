9d51dab381f452d959beaa9a1f75024f9b6cf8f5
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-22 22:46:34 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { createMiddlewareClient } from '@supabase/ssr' // pacote atualizado
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
+
   const {
     data: { user },
   } = await supabase.auth.getUser()
 
   const url = req.nextUrl.clone()
 
-  // Redireciona se o usuário não estiver logado e acessar rotas protegidas
   if (!user && url.pathname.startsWith('/dashboard')) {
     url.pathname = '/login'
     return NextResponse.redirect(url)
