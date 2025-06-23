806ad091116ee9ca66451243dfcb8e09b9ea1a18
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-23 10:24:18 -0300

    Update client.ts

--- app/lib/supabase/client.ts
+++ app/lib/supabase/client.ts
@@ -1,6 +1,3 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+export const supabase = createPagesBrowserClient();
