8cb70256d72f81c0186461d44cf7de1b83c5070a
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-22 22:34:21 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { createMiddlewareClient } from '@supabase/ssr' // pacote atualizado
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
-  const { data: { user } } = await supabase.auth.getUser()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
 
   const url = req.nextUrl.clone()
 
