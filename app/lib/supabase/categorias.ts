9b9f7332de9ece0d3edef9af6de138d876f3e2c8
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-23 10:42:03 -0300

    Update categorias.ts

--- app/lib/supabase/categorias.ts
+++ app/lib/supabase/categorias.ts
@@ -1,13 +1,12 @@
-'use server'
-
-import { supabase } from './server' // ou './client', dependendo do contexto
+import { getSupabaseServer } from './server'
 
 export async function buscarCategoriasPopulares() {
-  const { data, error } = await supabase.rpc('categorias_populares');
+  const supabase = getSupabaseServer()
+  const { data, error } = await supabase.rpc('categorias_populares')
 
   if (error) {
-    throw new Error("Erro ao buscar categorias: " + error.message);
+    throw new Error("Erro ao buscar categorias populares: " + error.message)
   }
 
-  return data as { categoria: string; count: number }[];
+  return data as { categoria: string; count: number }[]
 }
