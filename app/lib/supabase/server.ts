9ed36653dd340ca5f2b25b801306572989b35c63
rodrigosdiniz <rodrigosdiniz@gmail.com>
2025-06-22 22:20:04 -0300

    Update server.ts

--- app/lib/supabase/server.ts
+++ app/lib/supabase/server.ts
@@ -1,10 +1,10 @@
-import { createServerClient } from '@supabase/auth-helpers-nextjs'
+import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export const supabase = createServerClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
-    cookies: () => cookies(),
+    cookies,
   }
 )
